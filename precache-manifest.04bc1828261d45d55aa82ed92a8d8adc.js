caf9e98cc26308b255e35d6336486a7c21be62de
CrioDo <rahul@criodo.com>
2020-04-04 21:29:40 +0000

    Updates

---
+++ precache-manifest.04bc1828261d45d55aa82ed92a8d8adc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e735ed61eca21c0093901b2144cc243",
+    "url": "/Crio-Launch-Feb-2020-vaishnavtannu/index.html"
+  },
+  {
+    "revision": "770920549a85f0d63baa",
+    "url": "/Crio-Launch-Feb-2020-vaishnavtannu/static/css/2.57ddf7ad.chunk.css"
+  },
+  {
+    "revision": "67a25d1703891fd72111",
+    "url": "/Crio-Launch-Feb-2020-vaishnavtannu/static/css/main.5db1380d.chunk.css"
+  },
+  {
+    "revision": "770920549a85f0d63baa",
+    "url": "/Crio-Launch-Feb-2020-vaishnavtannu/static/js/2.daac8b4e.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/Crio-Launch-Feb-2020-vaishnavtannu/static/js/2.daac8b4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67a25d1703891fd72111",
+    "url": "/Crio-Launch-Feb-2020-vaishnavtannu/static/js/main.9e3f6c1f.chunk.js"
+  },
+  {
+    "revision": "e9bddba85cc0e5a4c6ae",
+    "url": "/Crio-Launch-Feb-2020-vaishnavtannu/static/js/runtime-main.23abc733.js"
+  },
+  {
+    "revision": "c9536e0497f939475da3110091e1a10d",
+    "url": "/Crio-Launch-Feb-2020-vaishnavtannu/static/media/crio-logo-white.c9536e04.svg"
+  },
+  {
+    "revision": "63be71d01edef2dcf3cafba5e52553a4",
+    "url": "/Crio-Launch-Feb-2020-vaishnavtannu/static/media/crio-logo.63be71d0.svg"
+  }
+]);
